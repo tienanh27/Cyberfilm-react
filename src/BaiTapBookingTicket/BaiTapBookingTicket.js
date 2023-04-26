a494752b6b5583f3fa5cab0781cddf22444989ce
tienanh27 <Anhtab0207@gmail.com>
2023-04-27 00:49:55 +0700

    Initial commit

---
+++ src/BaiTapBookingTicket/BaiTapBookingTicket.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './BaiTapBookingTicket.css';
+
+export default class BaiTapBookingTicket extends Component {
+  render() {
+    return (
+      <div>
+        hello
+
+      </div>
+    )
+  }
+}
